52054c1bb675a5650ca7ab95dff9ddf22bbf367e
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-09-15 05:10:01 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Irmalevi::Application.config.secret_key_base = '6f74ac55d9c5cb55531d016de4d6bf26daeb99bf756fb9019253088d87f32bc698abf740207b0c7455fc2ced6cc42077597d7b9969e95a8625569e2240f04957'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Irmalevi::Application.config.session_store :cookie_store, key: '_irmalevi_session'
